a7d54e0cc48e45c72d9486b01cba3129463c8330
Jazz Peh <jazzpehz@gmail.com>
2020-05-14 20:28:54 +0800

    Updated CounterRedux

--- src/pages/CounterRedux/CounterRedux.jsx
+++ src/pages/CounterRedux/CounterRedux.jsx
@@ -1,34 +1,81 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 import {
   incrementCounter,
   decrementCounter,
 } from '../../store/actions/counter';
 import Counter from '../../components/Counter';
 
+const Link = styled(NavLink)`
+  color: #61dafb;
+  font-size: calc(10px + 2vmin);
+  left: 5%;
+  margin-bottom: 20px;
+  position: fixed;
+  top: 5%;
+`;
+
+const Loader = styled.div`
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.8);
+  color: white;
+  display: flex;
+  font-size: 4rem;
+  font-weight: bold;
+  height: 100vh;
+  justify-content: center;
+  position: fixed;
+  width: 100vw;
+  z-index: 1;
+`;
+
 export class CounterRedux extends PureComponent {
   render() {
-    const { counter } = this.props;
+    const { counter, handleDecrement, handleIncrement, isLoading } = this.props;
 
-    return <Counter counter={counter} />;
+    return (
+      <>
+        {isLoading && <Loader>Loading...</Loader>}
+        <Link to="/" data-testid="home">
+          Back
+        </Link>
+        <Counter
+          data-testid="counter"
+          onClickDecrement={handleDecrement}
+          onClickIncrement={handleIncrement}
+          counter={counter}
+        />
+        ;
+      </>
+    );
   }
 }
 
 CounterRedux.propTypes = {
+  isLoading: PropTypes.bool,
   counter: PropTypes.number.isRequired,
+  handleDecrement: PropTypes.func.isRequired,
+  handleIncrement: PropTypes.func.isRequired,
+};
+
+CounterRedux.defaultProps = {
+  isLoading: false,
 };
 
 const mapStateToProps = (state) => {
   return {
+    isLoading: state.ui.isLoading,
     counter: state.counter.value,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onIncrementCounter: () => dispatch(incrementCounter()),
-    onDecrementCounter: () => dispatch(decrementCounter()),
+    handleIncrement: () => dispatch(incrementCounter()),
+    handleDecrement: () => dispatch(decrementCounter()),
   };
 };
 
--- src/pages/CounterRedux/CounterRedux.test.jsx
+++ src/pages/CounterRedux/CounterRedux.test.jsx
@@ -6,11 +6,11 @@ describe('CounterRedux', () => {
   let wrapper;
   let props;
 
-  beforeAll(() => {
+  beforeEach(() => {
     props = {
       counter: 0,
-      onIncrementCounter: jest.fn(),
-      onDecrementCounter: jest.fn(),
+      handleIncrement: jest.fn(),
+      handleDecrement: jest.fn(),
     };
 
     wrapper = shallow(<CounterRedux {...props} />);
@@ -18,5 +18,22 @@ describe('CounterRedux', () => {
 
   it('should render without errors', () => {
     expect(wrapper).toBeTruthy();
+  });
+
+  it('should render the correct link to home page', () => {
+    const link = wrapper.findByTestId('home');
+    expect(link.prop('to')).toEqual('/');
+  });
+
+  it('should call `handleIncrement` when clicked on increment button', () => {
+    wrapper.findByTestId('counter').props().onClickIncrement();
+    expect(props.handleIncrement.mock.calls.length).toBe(1);
+    expect(props.handleDecrement.mock.calls.length).toBe(0);
+  });
+
+  it('should call `handleDecrement` when clicked on decrement button', () => {
+    wrapper.findByTestId('counter').props().onClickDecrement();
+    expect(props.handleIncrement.mock.calls.length).toBe(0);
+    expect(props.handleDecrement.mock.calls.length).toBe(1);
   });
 });
